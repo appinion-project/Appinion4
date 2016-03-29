56658c69d6e077001511cde791d91b122971609c
TAMIM_ <appinion.tamim@gmail.com>
2016-03-29 18:09:12 +0600

    helo

--- app/src/main/java/com/appinion/app/MainActivity.java
+++ app/src/main/java/com/appinion/app/MainActivity.java
@@ -39,6 +39,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private EditText mSearchEt;
     public Button mHomebtn,mCouponbtn,mgiftcardbtn,mpromotionbtn;
 
+    int x=10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
